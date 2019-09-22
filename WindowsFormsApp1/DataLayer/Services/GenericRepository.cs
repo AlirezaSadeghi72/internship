3d38c626d94d1403f5da332570bd668a852dd7db
Alireza Sadeghi <dev.alirezasadeghi@gmail.com>
2019-09-22 15:20:22 +0330

    fix update class(genericRepuzitory)

--- WindowsFormsApp1/DataLayer/Services/GenericRepository.cs
+++ WindowsFormsApp1/DataLayer/Services/GenericRepository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1.DataLayer
 {
-    public class GenericRepository<T> where T : class 
+    public class GenericRepository<T> where T : class
     {
         private dbEntities _db;
         private DbSet<T> _dbSet;
@@ -20,27 +20,43 @@ namespace WindowsFormsApp1.DataLayer
             _db = db;
             _dbSet = _db.Set<T>();
         }
-        
 
-        public IEnumerable<T> get(Expression<Func<T, bool>> where = null, bool firstOrLirst = true, int? top = null , Expression<Func<T,object>> ali = null)
+
+        public IEnumerable<T> get(Expression<Func<T, bool>> where = null,
+            Expression<Func<T, object>> orderBy = null
+             , bool firstOrLirst = true, int? top = null)
         {
             IQueryable<T> result = _dbSet;
             if (where != null)
             {
                 result = result.Where(where);
             }
+
+            if (orderBy != null )
+            {
+                if (firstOrLirst)
+                    result = result.OrderBy(orderBy);
+                else
+                    result = result.OrderByDescending(orderBy);
+            }
+
+            if (top != null)
+            {
+                result = result.Take((int)top);
+            }
+
             return result.ToList();
         }
 
         public int? getMaxID(Expression<Func<T, int?>> max)
         {
-                return _dbSet.Max(max);
+            return _dbSet.Max(max);
         }
 
         public void insert(T Tentity)
-                {
-                    _dbSet.Add(Tentity);
-                }
-
-            }
+        {
+            _dbSet.Add(Tentity);
         }
+
+    }
+}
