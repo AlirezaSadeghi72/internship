d848ae952fd66438454381e701007ed48f48f32f
Alireza Sadeghi <dev.alirezasadeghi@gmail.com>
2019-09-22 17:54:39 +0330

    add Cod bind and search to datagredview

--- WindowsFormsApp1/Forms/frm_ShowCustomers.Designer.cs
+++ WindowsFormsApp1/Forms/frm_ShowCustomers.Designer.cs
@@ -28,37 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgv_customers = new System.Windows.Forms.DataGridView();
+            this.Select = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_customers)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgv_customers
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
-            this.dataGridView1.TabIndex = 0;
+            this.dgv_customers.AllowUserToAddRows = false;
+            this.dgv_customers.AllowUserToDeleteRows = false;
+            this.dgv_customers.BackgroundColor = System.Drawing.Color.White;
+            this.dgv_customers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_customers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Select});
+            this.dgv_customers.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgv_customers.Location = new System.Drawing.Point(0, 30);
+            this.dgv_customers.MultiSelect = false;
+            this.dgv_customers.Name = "dgv_customers";
+            this.dgv_customers.ReadOnly = true;
+            this.dgv_customers.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dgv_customers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_customers.Size = new System.Drawing.Size(800, 423);
+            this.dgv_customers.TabIndex = 0;
+            this.dgv_customers.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_customers_CellClick);
+            // 
+            // Select
+            // 
+            this.Select.FalseValue = "";
+            this.Select.HeaderText = "انتخاب";
+            this.Select.IndeterminateValue = "";
+            this.Select.Name = "Select";
+            this.Select.ReadOnly = true;
+            this.Select.TrueValue = "";
+            this.Select.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Location = new System.Drawing.Point(0, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(800, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // frm_ShowCustomers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgv_customers);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frm_ShowCustomers";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "لیست مشتری ها";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.frm_ShowCustomers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_customers)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgv_customers;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Select;
     }
 }
--- WindowsFormsApp1/Forms/frm_ShowCustomers.cs
+++ WindowsFormsApp1/Forms/frm_ShowCustomers.cs
@@ -7,14 +7,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.DataLayer;
 
 namespace WindowsFormsApp1.Forms
 {
     public partial class frm_ShowCustomers : Form
     {
+        private IEnumerable<CUSTOMERS> dt;
         public frm_ShowCustomers()
         {
             InitializeComponent();
         }
+
+        private void frm_ShowCustomers_Load(object sender, EventArgs e)
+        {
+            using (UnitOfWork db = new UnitOfWork())
+            {
+                dgv_customers.DataSource = dt = db.grCustomer.get(orderBy: c => c.SHMO, firstOrLirst: false, top: 10);
+            }
+
+            SetGrid();
+
+            dgv_customers.KeyPress += new KeyPressEventHandler(dataGridViewTextBox_KeyPress);
+        }
+
+        private void SetGrid()
+        {
+            foreach (DataGridViewColumn Col in dgv_customers.Columns)
+            {
+                Col.Visible = false;
+            }
+            dgv_customers.Columns["shmo"].Visible = true;
+            dgv_customers.Columns["shmo"].HeaderText = "شماره مشتری";
+
+            dgv_customers.Columns["moname"].Visible = true;
+            dgv_customers.Columns["moname"].HeaderText = "نام مشتری";
+
+            dgv_customers.Columns["addre"].Visible = true;
+            dgv_customers.Columns["addre"].HeaderText = "آدرس";
+
+            dgv_customers.Columns["Tell1"].Visible = true;
+            dgv_customers.Columns["Tell1"].HeaderText = "تلفن";
+
+            dgv_customers.Columns["cred"].Visible = true;
+            dgv_customers.Columns["cred"].HeaderText = "اعتبار";
+
+            dgv_customers.Columns["sharh"].Visible = true;
+            dgv_customers.Columns["sharh"].HeaderText = "توضیحات";
+
+            dgv_customers.Columns["maxmanfactor"].Visible = true;
+            dgv_customers.Columns["maxmanfactor"].HeaderText = "ت.ف.مجاز مانده";
+
+            dgv_customers.Columns["select"].Visible = true;
+        }
+
+        private void dgv_customers_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+
+                //dgv_customers.Columns["select"].Selected = true;
+                //if (dgv_customers.Rows[e.RowIndex].Cells["select"].Value == null)
+                //    dgv_customers.Rows[e.RowIndex].Cells["select"].Value = false;
+                //if ((bool)dgv_customers.Rows[e.RowIndex].Cells["select"].Value == false)
+                //{
+                //    dgv_customers.Rows[e.RowIndex].Cells["select"].Value = true;
+                //    dgv_customers.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.PaleTurquoise;
+                //}
+                //else
+                //{
+                //    dgv_customers.Rows[e.RowIndex].Cells["select"].Value = false;
+                //    dgv_customers.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.White;
+                //}
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.PaleTurquoise;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            textBox1.BackColor = SystemColors.Control;
+        }
+
+
+        private void dataGridViewTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            dgv_customers.Columns["select"].Selected = true;
+            if (dgv_customers.SelectedRows[0].Cells["select"].Value == null)
+                dgv_customers.SelectedRows[0].Cells["select"].Value = false;
+            if ((bool)dgv_customers.SelectedRows[0].Cells["select"].Value == false)
+            {
+                dgv_customers.SelectedRows[0].Cells["select"].Value = true;
+                dgv_customers.SelectedRows[0].DefaultCellStyle.BackColor = Color.PaleTurquoise;
+            }
+            else
+            {
+                dgv_customers.SelectedRows[0].Cells["select"].Value = false;
+                dgv_customers.SelectedRows[0].DefaultCellStyle.BackColor = Color.White;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                dgv_customers.DataSource = dt.Where(c => c.MONAME.Contains(textBox1.Text) || c.addre.Contains(textBox1.Text)).ToList();
+            }
+            else
+            {
+                dgv_customers.DataSource = dt;
+            }
+
+
+        }
     }
+
 }
